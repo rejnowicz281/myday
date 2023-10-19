aabd2238f6dd63537a76d941199af78271ed3601
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-19 21:55:53 +0200

    Run toString() on task.due_date in task show page

--- app/lists/[id]/components/Tasks/Task/index.js
+++ app/lists/[id]/components/Tasks/Task/index.js
@@ -9,7 +9,7 @@ export default function Task({ listId, task }) {
             <div>Last updated: {task.updatedAt.toString()}</div>
             <Completed listId={listId} taskId={task.id} completed={task.completed} />
             <MyDay listId={listId} taskId={task.id} my_day={task.my_day} />
-            {task.due_date && <div>Due: {task.due_date}</div>}
+            {task.due_date && <div>Due: {task.due_date.toString()}</div>}
             {task.repeat && <div>Repeating: every {task.repeat} day(s)</div>}
             {task.priority && <div>Priority: {task.priority}</div>}
             {task.note && <div>Note: {task.note}</div>}
