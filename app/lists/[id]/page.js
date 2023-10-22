9e1bc01dd7da4b26f09694da2692c098e1e0ddf9
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-22 17:45:39 +0200

    Format ListPage createdAt date

--- app/lists/[id]/page.js
+++ app/lists/[id]/page.js
@@ -1,5 +1,6 @@
 import { getList, updateListName } from "@actions/lists";
 import { createTask } from "@actions/tasks";
+import { formatDate } from "@utils/date";
 import AddTask from "../components/AddTask";
 import Tasks from "../components/Tasks";
 import DeleteButton from "./components/DeleteButton";
@@ -12,7 +13,7 @@ export default async function ListPage({ params: { id } }) {
         <>
             <EditableName action={updateListName} listId={id} name={list.name} />
             <DeleteButton listId={id} />
-            <p>{list.createdAt.toString()}</p>
+            <div>Created {formatDate(list.createdAt)}</div>
             <AddTask listId={id} action={createTask} />
             <Tasks listId={id} tasks={list.tasks} />
         </>
