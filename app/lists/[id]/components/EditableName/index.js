2b4eabe2b3f6cb5a4f7fb9399e7defe995e57cb2
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-28 19:17:30 +0200

    Make list name updating optimistic

--- app/lists/[id]/components/EditableName/index.js
+++ app/lists/[id]/components/EditableName/index.js
@@ -2,13 +2,19 @@
 
 import { updateListName } from "@/actions/lists";
 import ToggleEditable from "@/components/ToggleEditable";
+import { experimental_useOptimistic as useOptimistic } from "react";
 import css from "./index.module.css";
 
 export default function EditableName({ name, listId }) {
+    const [optimisticName, setOptimisticName] = useOptimistic(name);
+
     return (
         <ToggleEditable
-            action={(input) => updateListName(input, listId)}
-            display={name}
+            action={(input) => {
+                setOptimisticName(input);
+                updateListName(input, listId);
+            }}
+            display={optimisticName}
             submitContent="Update name"
             displayClass={css.display}
             inputClass={css.input}
