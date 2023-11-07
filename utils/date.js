8ca25c51493463e0db86d38dc0012751cc1dddda
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-11-07 22:32:07 +0100

    Create formatDateWithTime function

--- utils/date.js
+++ utils/date.js
@@ -28,3 +28,21 @@ export function formatDate(date) {
         ? "Tomorrow"
         : new_date;
 }
+
+export function formatDateWithTime(date) {
+    const temp = DateTime.fromJSDate(date);
+    const time = `${temp.hour < 10 ? "0" : ""}${temp.hour}:${temp.minute < 10 ? "0" : ""}${temp.minute}`;
+
+    const new_date = DateTime.fromJSDate(date).toFormat("yyyy-MM-dd");
+    const today = DateTime.now().toFormat("yyyy-MM-dd");
+    const tomorrow = DateTime.now().plus({ days: 1 }).toFormat("yyyy-MM-dd");
+    const yesterday = DateTime.now().plus({ days: -1 }).toFormat("yyyy-MM-dd");
+
+    return new_date == today
+        ? `Today | ${time}`
+        : new_date == yesterday
+        ? `Yesterday | ${time}`
+        : new_date == tomorrow
+        ? `Tomorrow | ${time}`
+        : `${new_date} | ${time}`;
+}
