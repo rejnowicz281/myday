8751a84935435b78cbfe91bd8fa2c3980bc9e034
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-11-08 21:58:46 +0100

    Add createdAt timestamp for list model

--- models/list.js
+++ models/list.js
@@ -1,16 +1,19 @@
 const { Schema, models, model } = require("mongoose");
 
-const listSchema = new Schema({
-    name: {
-        type: String,
-        default: "Untitled List",
-        required: true,
+const listSchema = new Schema(
+    {
+        name: {
+            type: String,
+            default: "Untitled List",
+            required: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            required: true,
+        },
     },
-    owner: {
-        type: Schema.Types.ObjectId,
-        required: true,
-    },
-});
+    { timestamps: { createdAt: true, updatedAt: false } }
+);
 
 const List = models.List || model("List", listSchema);
 
